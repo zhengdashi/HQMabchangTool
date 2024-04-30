dbefb7ac085a945995846828ece646d48ba49751
zhengdaqian <173977958@qq.com>
2024-04-30 17:00:17 +0800

    添加测试代码

---
+++ HQMabchangTool/Classes/HQTest.h
@@ -0,0 +1,16 @@
+//
+//  HQTest.h
+//  HQMabchangTool
+//
+//  Created by 郑浩然 on 2024/4/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HQTest : NSObject
+- (void)testPrint;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HQMabchangTool/Classes/HQTest.m
@@ -0,0 +1,14 @@
+//
+//  HQTest.m
+//  HQMabchangTool
+//
+//  Created by 郑浩然 on 2024/4/30.
+//
+
+#import "HQTest.h"
+
+@implementation HQTest
+- (void)testPrint {
+    NSLog(@"哈哈哈，我的pod的打印");
+}
+@end
