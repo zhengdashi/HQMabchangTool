83b92abfd24cd2df750daf7255ce3352c6e3de45
zhengdaqian <173977958@qq.com>
2024-05-06 11:23:16 +0800

    重新添加代码

---
+++ HQMabchangTool/Classes/HQMabchangHeader.h
@@ -0,0 +1,14 @@
+//
+//  HQMabchangHeader.h
+//  HQMabchangTool
+//
+//  Created by 郑浩然 on 2024/5/6.
+//
+
+#ifndef HQMabchangHeader_h
+#define HQMabchangHeader_h
+
+#import "HQTest.h"
+
+
+#endif /* HQMabchangHeader_h */
